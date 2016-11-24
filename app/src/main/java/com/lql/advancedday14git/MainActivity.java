f5d18ef8d48ed8f68e59a03e45f8563b0d13c6ba
wanghaixin <414348801@qq.com>
2016-11-24 18:36:13 +0800

    婚纱哈哈

--- app/src/main/java/com/lql/advancedday14git/MainActivity.java
+++ app/src/main/java/com/lql/advancedday14git/MainActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+/*
+*
+* 傻逼
+*
+* */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
